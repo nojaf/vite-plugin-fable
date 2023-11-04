3e7fb7962cc76838bf8e9aefd7c12af10383b1f5
nojaf <florian.verdonck@outlook.com>
2023-11-04 18:20:19 +0100

    Use in-memory streams for unit test.

--- Fable.Daemon.Tests/DebugTests.fs
+++ Fable.Daemon.Tests/DebugTests.fs
@@ -1,36 +1,19 @@
 module Fable.Daemon.Tests
 
+open System
 open System.Diagnostics
 open NUnit.Framework
+open Nerdbank.Streams
 open StreamJsonRpc
 open Fable.Daemon
 
 [<Test>]
 let DebugTest () =
     task {
-        let processStart =
-            let ps =
-                ProcessStartInfo (
-                    @"C:\Users\nojaf\Projects\vite-plugin-fable\Fable.Daemon\bin\Debug\net8.0\Fable.Daemon.exe"
-                )
-
-            ps.WorkingDirectory <- __SOURCE_DIRECTORY__
-            ps
-
-        processStart.UseShellExecute <- false
-        processStart.RedirectStandardInput <- true
-        processStart.RedirectStandardOutput <- true
-        processStart.RedirectStandardError <- true
-        processStart.CreateNoWindow <- true
-
-        let daemonProcess = Process.Start processStart
-
-        let client =
-            new JsonRpc (daemonProcess.StandardInput.BaseStream, daemonProcess.StandardOutput.BaseStream)
-
-        do client.StartListening ()
-
-        // Attach to process here.
+        let struct (serverStream, clientStream) = FullDuplexStream.CreatePair ()
+        let daemon = new Program.FableServer (serverStream, serverStream)
+        let client = new JsonRpc (clientStream, clientStream)
+        client.StartListening ()
 
         let! response =
             client.InvokeAsync<ProjectChangedResult> (
@@ -41,6 +24,9 @@ let DebugTest () =
                         @"C:\Users\nojaf\Projects\vite-plugin-fable\sample-project\node_modules\fable-library"
                 }
             )
+
+        client.Dispose ()
+        (daemon :> IDisposable).Dispose ()
 
         Assert.Pass ()
     }
