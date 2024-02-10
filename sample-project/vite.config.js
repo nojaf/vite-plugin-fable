e7e994164cfadb2409241db6e52c6c5305569cf6
nojaf <florian.verdonck@outlook.com>
2024-02-10 17:17:55 +0100

    Add link to setting

--- sample-project/vite.config.js
+++ sample-project/vite.config.js
@@ -1,18 +1,19 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import Inspect from "vite-plugin-inspect";
 import react from "@vitejs/plugin-react";
 import fable from "../index.js";
-import {fileURLToPath} from "node:url";
+import { fileURLToPath } from "node:url";
 import path from "node:path";
 
 const repositoryRoot = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), fable()],
-    server: {
-        fs: {
-            cachedChecks: false
-        }
-    }
+  plugins: [react(), fable()],
+  server: {
+    //https://github.com/vitejs/vite/issues/15784
+    fs: {
+      cachedChecks: false,
+    },
+  },
 });
