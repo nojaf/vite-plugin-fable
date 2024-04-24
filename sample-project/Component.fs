c53fbc536827517fd3209fc5d9193a4961501dc0
nojaf <florian.verdonck@outlook.com>
2024-04-24 07:10:44 +0000

    Further tweak dev container

--- sample-project/Component.fs
+++ sample-project/Component.fs
@@ -3,6 +3,15 @@ module Components.Component
 open Fable.Core
 open React
 open type React.DSL.DOMProps
+open type React.React
+
+JsInterop.importSideEffects "./app.css"
 
 [<ExportDefault>]
-let Component () : JSX.Element = div [] [ str "React rocks!" ]
+let Component () : JSX.Element =
+    let count, setCount = useStateByFunction (0)
+
+    fragment [] [
+        h1 [] [ str "React rocks!" ]
+        button [ OnClick (fun _ -> setCount ((+) 1)) ] [ str $"Current state {count}" ]
+    ]
--- sample-project/vite.config.js
+++ sample-project/vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 4000
+  },
   plugins: [
     Inspect(),
     fable({ jsx: "automatic" }),
